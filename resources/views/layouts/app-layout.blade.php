84656de8be89990faf34442031962e5ac0746d31
Andrey Andriansyah <andreyandri90@gmail.com>
2023-11-21 17:56:16 +0700

    update dikit 2

--- resources/views/layouts/app-layout.blade.php
+++ resources/views/layouts/app-layout.blade.php
@@ -23,7 +23,7 @@
         </nav>
         <!-- /.navbar -->
 
-        <x-sidebar menuActive="{{ $menuActive }}"></x-sidebar>
+        <x-sidebar menuActive="dashboard"></x-sidebar>
 
         <!-- Content Wrapper. Contains page content -->
         <div class="content-wrapper">
